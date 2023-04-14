ddfcebb4948fb3c07e48db309479755bf75e96ed
Breno Eustáquio <bx.sptf@gmail.com>
2023-04-14 12:16:25 -0300

    feat: route to get one agent and they specs

--- agents/control.py
+++ agents/control.py
@@ -1,41 +1,69 @@
 import requests
+from fastapi import HTTPException
 from agents.model import Agents
+from persons.model import Person, PersonFriends
 from settings import Base, engine, Session
 
+session = Session()
 
-def busca_agentes():
-    params = {
-        'language': 'pt-BR',
-        'isPlayableCharacter': True
-    }
-    response = requests.get('https://valorant-api.com/v1/agents', params=params)
-    data = response.json()
-    output = []
-    for agent in data["data"]:
-        abilities = agent["abilities"]
-        habilities_dict = {}
-        for i in range(5):
-            if i < len(abilities):
-                habilities_dict[f"ability_{i + 1}_name"] = abilities[i]["displayName"]
-                habilities_dict[f"ability_{i + 1}_description"] = abilities[i]["description"]
-            else:
-                habilities_dict[f"ability_{i + 1}_name"] = ""
-                habilities_dict[f"ability_{i + 1}_description"] = ""
 
-        output.append({
-            "agent_name": agent["displayName"],
-            "external_id": agent["uuid"],
-            "description": agent["description"],
-            "role": agent["role"]["displayName"],
-            **habilities_dict
-        })
-    return output
+class AgentsController:
+    id_agent: int
+
+    def __init__(self, id_agent: int = None):
+        self.id_agent = id_agent
+
+    @staticmethod
+    def get_or_update_agents():
+        params = {
+            'isPlayableCharacter': True
+        }
+        response = requests.get('https://valorant-api.com/v1/agents', params=params)
+        data = response.json()
+        output = []
+        for agent in data["data"]:
+            abilities = agent["abilities"]
+            habilities_dict = {}
+            for i in range(5):
+                if i < len(abilities):
+                    habilities_dict[f"ability_{i + 1}_name"] = abilities[i]["displayName"]
+                    habilities_dict[f"ability_{i + 1}_description"] = abilities[i]["description"]
+                else:
+                    habilities_dict[f"ability_{i + 1}_name"] = ""
+                    habilities_dict[f"ability_{i + 1}_description"] = ""
+
+            output.append({
+                "agent_name": agent["displayName"],
+                "external_id": agent["uuid"],
+                "description": agent["description"],
+                "role": agent["role"]["displayName"],
+                **habilities_dict
+            })
+        return output
+
+    def get_one_agent(self):
+        agent_query = session.query(Agents).filter_by(id=self.id_agent).first()
+        if agent_query is None:
+            raise HTTPException(status_code=404, detail="Agent not found")
+        response = {"name": agent_query.name,
+                    "role": agent_query.role,
+                    "ability_1_name": agent_query.ability_1_name,
+                    "ability_1_description": agent_query.ability_1_description,
+                    "ability_2_name": agent_query.ability_2_name,
+                    "ability_2_description": agent_query.ability_2_description,
+                    "ability_3_name": agent_query.ability_3_name,
+                    "ability_3_description": agent_query.ability_3_description,
+                    "ability_4_name": agent_query.ability_4_name,
+                    "ability_4_description": agent_query.ability_4_description,
+                    "ability_5_name": agent_query.ability_5_name,
+                    "ability_5_description": agent_query.ability_5_description,
+                    }
+        return response
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    session = Session()
-    data = busca_agentes()
+    data = AgentsController.get_or_update_agents()
     for agent_data in data:
         agent = Agents(
             name=agent_data['agent_name'],
@@ -54,4 +82,3 @@ if __name__ == '__main__':
         )
         session.add(agent)
     session.commit()
-
--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from agents.control import AgentsController
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/agents/{agent_id}")
+def get_individual_agent(agent_id: int):
+    agent = AgentsController(agent_id)
+    response = agent.get_one_agent()
+    return response
